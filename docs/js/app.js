b8e7e61d23d0e566dfe5cc7be095c351843b065c
jlerm13 <ja.lerman@ignitionapg.com>
2025-09-22 13:48:14 -0400

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,10 +1,1 @@
 // app.js
-document.addEventListener('DOMContentLoaded', () => {
-  const testOutput = document.getElementById('test-output');
-  if (!testOutput) return;
-
-  const hypertrophyTemplate = trainingTemplates["Hypertrophy Upper"];
-
-  testOutput.textContent = `Loaded Template: ${hypertrophyTemplate.name}`;
-});
-
