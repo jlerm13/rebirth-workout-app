1e62cdeef46cc62fceb43a4901d360ec06a975da
jlerm13 <ja.lerman@ignitionapg.com>
2025-09-23 15:50:26 -0400

    Create beginner.js

---
+++ docs/js/templates/beginner.js
@@ -0,0 +1,18 @@
+(function() {
+    const beginnerTemplates = {
+        beginner: {
+            'early-offseason': {
+                '4day': {
+                    week1: {
+                        // Copy your beginner week 1 workouts here
+                    },
+                    week2: {
+                        // Copy your beginner week 2 workouts here
+                    }
+                }
+            }
+        }
+    };
+    
+    safeLoadTemplates('beginner', beginnerTemplates);
+})();
