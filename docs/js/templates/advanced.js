c30db93bddf83426614aa24ead4ee2dd8e5112a2
jlerm13 <ja.lerman@ignitionapg.com>
2025-09-23 16:04:46 -0400

    Update advanced.js

--- docs/js/templates/advanced.js
+++ docs/js/templates/advanced.js
@@ -1,1 +1,18 @@
-
+(function() {
+    const advancedTemplates = {
+        advanced: {
+            'early-offseason': {
+                '4day': {
+                    week1: {
+                        // Copy your beginner week 1 workouts here
+                    },
+                    week2: {
+                        // Copy your beginner week 2 workouts here
+                    }
+                }
+            }
+        }
+    };
+    
+    safeLoadTemplates('advanced', advancedTemplates);
+})();
